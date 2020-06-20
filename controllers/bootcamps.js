1e40656d627e571531d52f0439421d5a998dd385
ishvar99 <ishanvarshney99@gmail.com>
2020-06-20 17:55:30 +0530

    added description for bootcamp controllers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,17 +1,34 @@
+// @desc    Get all bootcamps
+// @route   /api/v1/bootcamps
+// @access  public
 exports.getBootcamps = (req, res) => {
   res.send('fetch all bootcamps');
 };
 
+// @desc    Get all bootcamps
+// @route   /api/v1/bootcamps
+// @access  public
 exports.getBootcamp = (req, res) => {
   res.send('fetch single bootcamp');
 };
 
+// @desc    Get all bootcamps
+// @route   /api/v1/bootcamps
+// @access  private
 exports.createBootcamp = (req, res) => {
   res.send('create bootcamp');
 };
+
+// @desc    Get all bootcamps
+// @route   /api/v1/bootcamps
+// @access  private
 exports.updateBootcamp = (req, res) => {
   res.send('update bootcamp');
 };
+
+// @desc    Get all bootcamps
+// @route   /api/v1/bootcamps
+// @access  private
 exports.deleteBootcamp = (req, res) => {
   res.send('delete bootcamp');
 };
