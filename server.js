06a1dea88100b37c035877f921aedc28c56986a5
ishvar99 <ishanvarshney99@gmail.com>
2020-06-21 00:08:23 +0530

    configured mongoDB

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const PORT = process.env.PORT;
 const bootCampRoutes = require('./routes/bootcamps');
+const connectDB = require('./database/db');
 const morgan = require('morgan');
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
+connectDB();
 app.use('/api/v1/bootcamps', bootCampRoutes);
 
 app.listen(PORT, () => {
