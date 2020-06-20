f14378fb5e3f47ed60eeb66e2f434c8973f94c1e
ishvar99 <ishanvarshney99@gmail.com>
2020-06-20 23:24:37 +0530

    setup HTTP request logger middleware

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const PORT = process.env.PORT;
 const bootCampRoutes = require('./routes/bootcamps');
-
+const morgan = require('morgan');
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
 app.use('/api/v1/bootcamps', bootCampRoutes);
 
 app.listen(PORT, () => {
