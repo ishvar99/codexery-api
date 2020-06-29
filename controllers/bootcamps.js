c85c833916a39c8620c798c8b39dcc1c1d3f77fc
ishvar99 <ishanvarshney99@gmail.com>
2020-06-29 14:09:45 +0530

    added a asyncHandler to remove try catch

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,44 +1,37 @@
 const Bootcamp = require('../models/bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middlewares/asyncHandler');
 // @desc    Get all bootcamps
 // @route   /api/v1/bootcamps
 // @access  public
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find({});
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find({});
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
 
 // @desc    Get all bootcamps
 // @route   /api/v1/bootcamps
 // @access  public
-exports.getBootcamp = async (req, res, next) => {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const id = req.params.id;
-  try {
-    const bootcamp = await Bootcamp.findById(id);
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
-    }
-    res.status(200).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
+  const bootcamp = await Bootcamp.findById(id);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
   }
-};
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Get all bootcamps
 // @route   /api/v1/bootcamps
 // @access  private
-exports.createBootcamp = async (req, res, next) => {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
   // const body = [];
   // req.on('data', (chunk) => {
   //   body.push(chunk);
@@ -47,48 +40,36 @@ exports.createBootcamp = async (req, res, next) => {
   //   const parsedBody = Buffer.concat(body).toString();
   //   console.log(parsedBody);
   // });
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Get all bootcamps
 // @route   /api/v1/bootcamps
 // @access  private
-exports.updateBootcamp = async (req, res) => {
+exports.updateBootcamp = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  try {
-    const updatedBootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!updatedBootcamp) {
-      return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
-    }
-    res.status(200).json({ success: true, data: updatedBootcamp });
-  } catch (error) {
-    next(error);
+  const updatedBootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!updatedBootcamp) {
+    return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
   }
-};
+  res.status(200).json({ success: true, data: updatedBootcamp });
+});
 
 // @desc    Get all bootcamps
 // @route   /api/v1/bootcamps
 // @access  private
-exports.deleteBootcamp = async (req, res) => {
+exports.deleteBootcamp = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  try {
-    const deletedBootcamp = await Bootcamp.findByIdAndDelete(id);
-    if (!deletedBootcamp) {
-      return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
-    }
-    res.json({ success: true, data: {} });
-  } catch (error) {
-    next(error);
+  const deletedBootcamp = await Bootcamp.findByIdAndDelete(id);
+  if (!deletedBootcamp) {
+    return next(new ErrorResponse(`Cannot find resource with id ${id}`, 404));
   }
-};
+  res.json({ success: true, data: {} });
+});
