42ad1a9665bc502a4282d46ec1302735eb463643
ishvar99 <ishanvarshney99@gmail.com>
2020-06-30 22:54:22 +0530

    add mongoose middleware to compute average cost of bootcamp when course is added or removed

--- models/course.js
+++ models/course.js
@@ -37,5 +37,29 @@ const courseSchema = new mongoose.Schema({
     required: true,
   },
 });
-
+courseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log('Calculating Average Cost...'.blue);
+  const obj = await this.aggregate([
+    { $match: { bootcamp: bootcampId } },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+courseSchema.post('save', function () {
+  this.model('Course').getAverageCost(this.bootcamp);
+});
+courseSchema.pre('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
 module.exports = mongoose.model('Course', courseSchema);
