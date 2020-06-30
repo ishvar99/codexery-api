7ec2da033fb4cac123cba6afc5866a3fedd02ee4
ishvar99 <ishanvarshney99@gmail.com>
2020-07-01 01:08:25 +0530

    extended middleware logic to courses controllers

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
+const advancedResults = require('../middlewares/advancedResults');
+const Course = require('../models/course');
 const {
   getCourses,
   getCourse,
@@ -9,7 +10,16 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
-router.route('/').get(getCourses).post(addCourse);
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getCourses
+  )
+  .post(addCourse);
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
